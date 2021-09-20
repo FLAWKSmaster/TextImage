8c8779fb26bb339d32d7ea6f8911684a1e1d3504
chaikav_vadim@mail.ru <chaikav_vadim@mail.ru>
2021-09-20 23:49:48 +0200

    Доведено до функционала

---
+++ TextImageApp/Controllers/DownloadController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TextImageApp.Controllers
+{
+    public class DownloadController
+    {
+    }
+}
